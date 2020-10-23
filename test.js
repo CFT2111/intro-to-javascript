69059f905e178bbe246d0aedff35d2cb953853b4
M Mantle <mmantlehud@googlemail.com>
2020-10-23 10:08:50 +0100

    Corrected the instructions

--- test.js
+++ test.js
@@ -7,8 +7,5 @@ document.body.style.backgroundColor="green";
 // Produces a prompt box that asks the user for their name
 prompt("What is your name");
 
-// Changes the location of the page to the university home page
-//window.location="http://www.hud.ac.uk"; //this is commented out so we can see the effect of the other lines of code.
-
 // Using console.log() output the following text.
 console.log("The University of Huddersfield is a dynamic and expanding institution in a thriving West Yorkshire town.")

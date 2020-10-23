5f4a08afb4f58b6428a62071b51ddd5a3b8ab3a2
M Mantle <mmantlehud@googlemail.com>
2020-10-23 10:08:34 +0100

    Corrected the instructions

---
+++ test.js
@@ -0,0 +1,14 @@
+//Produces an alert box containing the text 'JavaScript is Great'
+alert("JavaScript is great");
+
+// Changes the background colour of the page to green
+document.body.style.backgroundColor="green";
+
+// Produces a prompt box that asks the user for their name
+prompt("What is your name");
+
+// Changes the location of the page to the university home page
+//window.location="http://www.hud.ac.uk"; //this is commented out so we can see the effect of the other lines of code.
+
+// Using console.log() output the following text.
+console.log("The University of Huddersfield is a dynamic and expanding institution in a thriving West Yorkshire town.")
